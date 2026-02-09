08ee1faa5b7c2a576026bc57c7df000a483f0e03
Umesh C <uchaurasiya@girsoftwareservices.com>
2026-02-09 14:18:19 +0530

    production build issue

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -23,11 +23,15 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT),
-      secure: false,
+      secure: Number(process.env.SMTP_PORT) === 465,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+
+      connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
     });
 
     // ✅ Email Send
